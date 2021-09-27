65031ecad7f078981e1dd9ab9411d837e59ff588
scrof90 <asknyshev@gmail.com>
2021-09-28 02:08:31 +0300

    add function for bookshelf updating

--- main.js
+++ main.js
@@ -4,12 +4,34 @@ class Book {
   constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
     this.read = read;
+  }
+  setID() {
+    this.id = library.indexOf(this);
+    return this;
+  }
+  getHTMLTemplate() {
+    return `<p>${this.title}<p>
+    <p>by<p>
+    <p>${this.author}<p>
+    <p>${this.read ? 'read' : 'not read'}<p>`;
   }
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  const newBook = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, read) {
+  const newBook = new Book(title, author, read);
   library.push(newBook);
+  newBook.setID();
+  updateBookshelf();
 }
+
+function updateBookshelf() {
+  library.forEach((book) => {
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+    bookCard.innerHTML = book.getHTMLTemplate();
+    document.querySelector('.js-bookshelf').appendChild(bookCard);
+  });
+}
+
+addBookToLibrary('Zlaty Troll', 'O.S. Leshev', 100, true);
