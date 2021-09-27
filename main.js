d614d9cc32c72e62e0625ce0895abe4b0a4dfdf0
scrof90 <asknyshev@gmail.com>
2021-09-28 01:15:47 +0300

    add initial styling and Book class

---
+++ main.js
@@ -0,0 +1,15 @@
+let library = [];
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  library.push(newBook);
+}
